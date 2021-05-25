c9b65929f41dcde946853dfc6205aa535b228f55
arednaya <antonina.rednaya@gmail.com>
2021-05-25 16:25:09 +0500

    Gallery added

---
+++ script.js
@@ -0,0 +1,10 @@
+let pic1 = document.getElementById('pic1');
+let pic2 = document.getElementById('pic2');
+function changePicLeft() {
+  pic1.src = 'https://pbs.twimg.com/profile_images/3607997687/771fab38b10be0d599d97d09a3fd4902.jpeg';
+  pic2.src = 'https://image1.thematicnews.com/uploads/topics/preview/00/04/42/42/c2a13385fa_256crop.jpg';
+}
+function changePicRight() {
+  pic1.src = 'https://pbs.twimg.com/profile_images/3297169579/d5d439c8cbfefccfa4ab9e799fbd507d.jpeg';
+  pic2.src = 'https://pbs.twimg.com/profile_images/3607997687/771fab38b10be0d599d97d09a3fd4902.jpeg';
+}
